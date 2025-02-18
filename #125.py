f2a144a6b2490df37a84c018ce6211d6d8b14fe6
Hidhayath Nisha Mohamed Idris <66171117+Hidhayath-Nisha@users.noreply.github.com>
2025-02-18 17:50:47 -0500

    Valid Palindrome

---
+++ #125.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = ''.join(filter(str.isalnum, s)) # to remove all the non-alphanumberic characters from the string
+        s = s.lower()
+        for i in range(0, len(s)//2):
+            if s[i] == s[len(s)-i-1]:
+                continue
+            else:
+                return False
+        return True
