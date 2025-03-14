65b17094210f2e5f2b8b10594c9cfdd12df4a91e
Hidhayath Nisha Mohamed Idris <66171117+Hidhayath-Nisha@users.noreply.github.com>
2025-03-13 23:59:55 -0400

    Update #1657.py

--- Leetcode-75 /#1657.py
+++ Leetcode-75 /#1657.py
@@ -1,1 +1,12 @@
-
+from collections import Counter
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        if len(word1) != len(word2):
+            return False
+        s1 = set(word1)
+        s2 = set(word2)
+        if s1 != s2:
+            return False
+        c1 = Counter(word1)
+        c2 = Counter(word2)
+        return c1.values() == c2.values()
