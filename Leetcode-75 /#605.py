4386f5280cc47a6eddbeb15b6f6804f6fc3abe3e
Hidhayath Nisha Mohamed Idris <66171117+Hidhayath-Nisha@users.noreply.github.com>
2025-03-15 19:01:42 -0400

    Can Place Flowers

---
+++ Leetcode-75 /#605.py
@@ -0,0 +1,19 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        l = len(flowerbed)
+        for i in range(0, l): 
+            if flowerbed[i] == 1: 
+                continue
+            else:
+                if l == 1 or n == 0:
+                    return True
+                elif i == 0 and flowerbed[i+1] == 0:
+                    n = n - 1
+                    flowerbed[i] = 1
+                elif i == l-1 and flowerbed[i-1] == 0: 
+                    n = n - 1
+                    flowerbed[i] = 1
+                elif flowerbed[i-1] == 0 and flowerbed[i+1] == 0:
+                    n = n - 1
+                    flowerbed[i] = 1
+        return n == 0
