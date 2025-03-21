57366de7bed1df0e881f6e5e07c72bbe81e2825d
Hidhayath Nisha Mohamed Idris <66171117+Hidhayath-Nisha@users.noreply.github.com>
2025-03-21 18:04:20 -0400

    Find the Middle Index in Array

---
+++ #1991.py
@@ -0,0 +1,13 @@
+class Solution:
+    def findMiddleIndex(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefixSum = [0] * n
+        suffixSum = [0] * n
+        for i in range(1, n):
+            prefixSum[i] = prefixSum[i-1] + nums[i-1]
+        for i in range(n-2, -1, -1):
+            suffixSum[i] = suffixSum[i+1] + nums[i+1] 
+        for i in range(n):
+            if prefixSum[i] == suffixSum[i]:
+                return i
+        return -1
