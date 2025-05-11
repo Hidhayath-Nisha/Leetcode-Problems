9383e67648ae47cc1e19678f637cb971449087d6
Hidhayath Nisha Mohamed Idris <66171117+Hidhayath-Nisha@users.noreply.github.com>
2025-05-11 00:08:01 -0400

    Maximum Number of Vowels in a Substring of Given Length

--- Leetcode-75 /#1456.py
+++ Leetcode-75 /#1456.py
@@ -3,8 +3,11 @@ class Solution:
         # -- Brute Force --
         # Loop - split the array in k size - loop again and each word should be in vowels plus one else skip 
         # Two pointers and sliding windoW
-        window = s[:k]
-        windowCount = window.count('a') + window.count('e') + window.count('i') + window.count('o') + window.count('u')
+
+        # window = s[:k]
+        # windowCount = window.count('a') + window.count('e') + window.count('i') + window.count('o') + window.count('u')
+        # alternate way to get initial substring's vowels count
+        windowCount = sum([1 if i in ['a','e','i','o','u'] else 0 for i in s[:k]])
         maxCount = windowCount
         for i in range(k,len(s)):
             if s[i-k] in ['a','e','i','o','u']:
